4f7f07f2b10928a82f3b7f9a1a2a4552f583d045
John Fecko <john@fecko.me>
2025-06-04 16:04:37 -0400

    Update page styling and functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import FormattedPhoneNumber from "@/components/formattedPhoneNumber";
+import SortableColumnHeader from "@/components/sortableColumnHeader";
 import Advocate from "@/interfaces/advocate";
 import advocateService from "@/services/advocateService";
 import { ChangeEvent, useEffect, useState } from "react";
@@ -7,7 +9,8 @@ import { ChangeEvent, useEffect, useState } from "react";
 export default function Home() {
   const [advocates, setAdvocates] = useState<Array<Advocate>>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
-
+  const [sortColumn, setSortColumn] = useState<string>(''); // Default to sorting by the first column (Name)
+  const [sortDirection, setSortDirection] = useState<number>(1); // Default to ascending order
 
 
   useEffect(() => {
@@ -18,56 +21,69 @@ export default function Home() {
 
   }, []);
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>): void => {
+  const searchOnChange = (e: ChangeEvent<HTMLInputElement>): void => {
     setSearchTerm(e.target.value);
   };
 
-  const onClick = (): void => {
+  const resetSearchClick = (): void => {
     setSearchTerm("");
   };
 
+  const onColumnSort = (column: string): void => {
+    let direction = -sortDirection;
+    if (sortColumn === '' || column !== sortColumn) {
+      direction = 1;
+    }
+
+    let sorted = advocates.toSorted((a: Advocate, b: Advocate) => {
+      switch (column) {
+        case 'name': // Name
+          return a.lastName.localeCompare(b.lastName) * direction;
+
+        case 'experience':
+          return (a.yearsOfExperience > b.yearsOfExperience ? 1 : -1) * -direction;
+      }
+      return 1;
+    });
+    setAdvocates(sorted);
+    setSortColumn(column);
+    setSortDirection(direction);
+  }
+
+
+
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term">{searchTerm}</span>
-        </p>
-        <input style={{ border: "1px solid black" }} value={searchTerm} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+    <main className="p-12 font-sans">
+      <h1 className="font-[Solace] text-4xl text-center">Solace Advocates</h1>
+      <div className="flex justify-end">
+        <div>
+          <input placeholder="Search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 mr-2 " value={searchTerm} onChange={searchOnChange} />
+          <button onClick={resetSearchClick} className="text-white bg-[#285e50] border border-gray-300 focus:outline-none hover:bg-[#347866] focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Reset</button>
+        </div>
       </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
+      <table className="w-full text-[#333] bg-[#d4e2dd40] ">
+        <thead className="bg-[#3f937c] text-white">
+          <tr className="h-8">
+            <SortableColumnHeader columnName="Name" onColumnSort={() => onColumnSort('name')} />
             <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
+            <SortableColumnHeader columnName="Years of Experience" onColumnSort={() => onColumnSort('experience')} />
             <th>Phone Number</th>
+            <th className="w-2/5">Specialties</th>
           </tr>
         </thead>
         <tbody>
           {advocates.map((advocate) => {
             return (advocateService.isSearchMatch(advocate, searchTerm) &&
-              <tr key={advocate.id}>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
+              <tr key={advocate.id} className="text-center even:bg-gray-200 odd:bg-white">
+                <td>{advocate.firstName} {advocate.lastName} {advocate.degree}</td>
                 <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
+                <td>{advocate.yearsOfExperience}</td>
+                <td><FormattedPhoneNumber phoneNumber={advocate.phoneNumber} /></td>
+                <td className="flex flex-wrap justify-center gap-2">
                   {advocate.specialties.map((s, index) => (
-                    <div key={index}>{s}</div>
+                    <span key={index} className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-gray-500/10 ring-inset">{s}</span>
                   ))}
                 </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
               </tr>
             );
           })}
